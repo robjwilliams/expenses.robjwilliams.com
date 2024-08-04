133b870995e0e60af775382c1febf95dd611da91
robjwilliams <robert151196@gmail.com>
2024-08-04 11:35:53 -0300

    updates bot for testing

--- scripts/carrefour/bot.js
+++ scripts/carrefour/bot.js
@@ -1,19 +1,23 @@
-import puppeteer from "puppeteer";
+const puppeteer = require("puppeteer");
+// import puppeteer from "puppeteer";
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const startBrowser = async () => {
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless: true,
     args: [
+      // "ignoreHTTPSErrors",
+      // "--single-process",
+      // "--disable-setuid-sandbox",
       "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
+      // "--disable-dev-shm-usage",
     ],
+    // executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    // devtools: false,
+    // dumpio: true,
   });
   const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(90000);
-  page.setDefaultTimeout(30000);
   page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
   page.on("error", (err) => console.error("PAGE ERROR:", err));
   page.on("pageerror", (pageErr) => console.error("PAGE ERROR:", pageErr));
@@ -25,56 +29,63 @@ const login = async (page) => {
   try {
     console.log("Navigating to the login page...");
     await page.goto("https://www.micarrefour.com.ar/", {
-      waitUntil: "networkidle0",
-      timeout: 90000,
+      waitUntil: "domcontentloaded",
+      timeout: 60000,
     });
 
     console.log("Clicking on the login button...");
-    await page.waitForSelector(".ingresar_acceder_hogar", { visible: true });
-    await page.click(".ingresar_acceder_hogar");
+    await page.locator(".ingresar_acceder_hogar").click();
 
-    await delay(5000);
+    // await delay(20000);
 
     console.log("Taking screenshot before login button click...");
     await page.screenshot({ path: "before_login_button_click.png" });
 
     console.log("Waiting for email input to be visible...");
-    await page.waitForSelector("#email", { visible: true });
-    await page.type("#email", process.env.CARREFOUR_EMAIL);
+    await page.waitForSelector("#email", { visible: true, timeout: 60000 }); // Increase timeout to 20 seconds
+    await page.locator("#email").fill(process.env.EMAIL);
 
     console.log("Waiting for password input to be visible...");
-    await page.waitForSelector("#password", { visible: true });
-    await page.type("#password", process.env.CARREFOUR_PASSWORD);
+    await page.waitForSelector("#password", { visible: true, timeout: 60000 }); // Increase timeout to 20 seconds
+    await page.locator("#password").fill(process.env.PASSWORD);
 
     console.log("Taking screenshot after filling email and password...");
     await page.screenshot({ path: "after_filling_email_and_password.png" });
 
     console.log("Submitting login form...");
-    await Promise.all([
-      page.waitForNavigation({ waitUntil: "networkidle0" }),
-      page.click('#login-form button[type="submit"]'),
-    ]);
+    await page.waitForSelector('#login-form button[type="submit"]', {
+      visible: true,
+      timeout: 20000, // Increase timeout to 20 seconds
+    });
+    await page.locator('#login-form button[type="submit"]').click();
 
-    console.log("Login completed.");
-    await page.screenshot({ path: "after_login.png" });
+    console.log("Waiting for login to complete...");
   } catch (error) {
     console.error("Error during login:", error);
     const screenshotPath = "error_screenshot.png";
     await page.screenshot({ path: screenshotPath });
     console.log(`Screenshot of the error saved at ${screenshotPath}`);
-    throw error;
   }
 };
 
+// export const navigateToBilling = async (page) => {
+//   console.log("Waiting for 'Mis Consumos' element to be visible...");
+
+//   await page.locator('li[class*="consumos"]').click();
+
+//   console.log("Waiting for navigation...");
+//   await page.waitForNavigation({ waitUntil: "networkidle0" });
+// };
+
 (async () => {
   const { browser, page } = await startBrowser();
-  try {
-    await login(page);
-    console.log("Logged in successfully.");
-  } catch (error) {
-    console.error("Script failed:", error);
-    process.exit(1);
-  } finally {
-    await browser.close();
-  }
+  await login(page);
+  await page.screenshot({ path: "after_login.png" });
+  console.log("Logged in successfully.");
+  // setTimeout(() => {}, 2000);
+  // await navigateToBilling(page);
+  // setTimeout(() => {}, 2000);
+  // await page.screenshot({ path: "after_navigate.png" });
+
+  await browser.close();
 })();
