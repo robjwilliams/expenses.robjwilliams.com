fe53ceff876b7424b0b5f181b0d76673d9e94ca0
robjwilliams <robert151196@gmail.com>
2024-08-11 13:30:53 -0300

    fix default date range

--- app/(main)/overview/page.tsx
+++ app/(main)/overview/page.tsx
@@ -5,7 +5,7 @@ import OverviewClient from "./OverviewClient";
 
 function getCurrentMonthDateRange() {
   const now = new Date();
-  const start = new Date(now.getFullYear(), now.getMonth(), 1);
+  const start = new Date(now.getFullYear(), now.getMonth(), 2);
   const end = new Date(now.getFullYear(), now.getMonth() + 1, 1);
   return {
     from: start.toISOString().split("T")[0],
