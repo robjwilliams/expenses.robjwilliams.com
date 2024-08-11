90d49f30b107ccdafe3106d3b17229dfc11d1ce1
robjwilliams <robert151196@gmail.com>
2024-08-11 13:31:04 -0300

    updates redirect after login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default async function Login({
       return redirect("/login?message=Could not authenticate user");
     }
 
-    return redirect("/");
+    return redirect("/purchases");
   };
 
   return (
